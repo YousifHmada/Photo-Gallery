1a27c53b65f290da625b4ebea9f56d9e92bf2cbd
YousifHmada <1401724@eng.asu.edu.eg>
2017-11-13 18:31:14 +0200

    =adding the user class

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,8 @@
 <?php
 
 // Database Constants
-defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "gallery");
-defined('DB_PASS')   ? null : define("DB_PASS", "phpOTL123");
-defined('DB_NAME')   ? null : define("DB_NAME", "photo_gallery");
-
+defined('DB_SERVER') ? null : define('DB_SERVER', 'localhost');
+defined('DB_NAME') ? null : define('DB_NAME', 'photo_gallery');
+defined('DB_USER') ? null : define('DB_USER', 'photoAdmin');
+defined('DB_PASS') ? null : define('DB_PASS', 'hashed');
 ?>
